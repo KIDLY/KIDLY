0050f760cd9558268df24588badf9fd1a49b0568
dotto <f74986159@mail.ncku.edu.tw>
2013-01-17 01:46:24 +0800

    Add input New Text function.

--- src/KidlyGui/AddText.java
+++ src/KidlyGui/AddText.java
@@ -3,13 +3,15 @@ package KidlyGui;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.FlowLayout;
+import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
 import java.awt.image.BufferedImage;
-import java.util.ArrayList;
 
 import javax.swing.DefaultComboBoxModel;
 import javax.swing.ImageIcon;
@@ -20,7 +22,7 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JSeparator;
-import javax.swing.JTextArea;
+import javax.swing.JTextField;
 import javax.swing.JToggleButton;
 import javax.swing.border.EmptyBorder;
 
@@ -29,7 +31,15 @@ public class AddText extends JDialog {
 	private JPanel contentPanel = new JPanel();
 	private BufferedImage buffer = null;
 	private Graphics2D bg;
-	private JTextArea textArea;
+	private TextPanel result_panel;
+	private JTextField textArea;
+	private JTextField textField;
+	private JToggleButton italicToggleButton, UnderlineToggleButton, boldToggleButton;
+	private Font textFont;
+	private int textSize;
+	private String textType;
+	private JComboBox comboBox_Size, comboBox_Type;
+
 	/**
 	 * Launch the application.
 	 */
@@ -55,75 +65,100 @@ public class AddText extends JDialog {
 		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
 		getContentPane().add(contentPanel, BorderLayout.CENTER);
 		contentPanel.setLayout(null);
-		
+
 		JLabel lblText = new JLabel("Text:");
-		lblText.setBounds(10, 28, 46, 15);
+		lblText.setBounds(25, 28, 65, 15);
 		contentPanel.add(lblText);
-		
+
 		JLabel lblNewLabel = new JLabel("Font Size:");
-		lblNewLabel.setBounds(261, 28, 80, 15);
+		lblNewLabel.setBounds(26, 92, 65, 15);
 		contentPanel.add(lblNewLabel);
 		
-		JComboBox comboBox = new JComboBox();
-		comboBox.setModel(new DefaultComboBoxModel(new String[] {"8", "10", "12", "14", "16", "18", "20", "36", "48", "72"}));
-		comboBox.setBounds(351, 28, 65, 21);
-		contentPanel.add(comboBox);
 		
+		
+		comboBox_Size = new JComboBox();
+		comboBox_Size.addItemListener(new ItemListener() {
+			public void itemStateChanged(ItemEvent e) {
+				textSize = Integer.valueOf(comboBox_Size.getSelectedItem().toString());
+
+			}
+		});
+		comboBox_Size.setModel(new DefaultComboBoxModel(new String[] { "8", "10", "12", "14", "16", "18", "20", "36", "48", "72" }));
+		comboBox_Size.setSelectedIndex(2);
+		comboBox_Size.setBounds(101, 89, 65, 21);
+		contentPanel.add(comboBox_Size);
+
 		JLabel lblFontType = new JLabel("Font Type:");
-		lblFontType.setBounds(261, 58, 80, 15);
+		lblFontType.setBounds(26, 122, 65, 15);
 		contentPanel.add(lblFontType);
+
+		comboBox_Type = new JComboBox();
+		comboBox_Type.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				textType = comboBox_Type.getSelectedItem().toString();
+			}
+		});
+		comboBox_Type.setModel(new DefaultComboBoxModel(new String[] {"SansSerif", "Serif", "Dialog", "DialogInput", "Monospaced"}));
+		comboBox_Type.setBounds(101, 123, 108, 21);
+		contentPanel.add(comboBox_Type);
+
+		boldToggleButton = new JToggleButton("");
+		boldToggleButton.addActionListener(btnAction);
+	
+		boldToggleButton.setIcon(new ImageIcon(AddText.class.getResource("/res/bold.png")));
+		boldToggleButton.setBounds(352, 92, 26, 23);
+		contentPanel.add(boldToggleButton);
+
+		italicToggleButton = new JToggleButton("");
+		italicToggleButton.addActionListener(btnAction);
 		
-		JComboBox comboBox_1 = new JComboBox();
-		comboBox_1.setModel(new DefaultComboBoxModel(new String[] {"SansSerif", "Serif", "Dialog", "DialogInput", "Monospaced"}));
-		comboBox_1.setBounds(351, 59, 108, 21);
-		contentPanel.add(comboBox_1);
+		italicToggleButton.setIcon(new ImageIcon(AddText.class.getResource("/res/italic.png")));
+		italicToggleButton.setBounds(388, 92, 26, 23);
+		contentPanel.add(italicToggleButton);
+
+		UnderlineToggleButton = new JToggleButton("");
+		UnderlineToggleButton.addActionListener(btnAction);
 		
-		JToggleButton tglbtnNewToggleButton = new JToggleButton("");
-		tglbtnNewToggleButton.setIcon(new ImageIcon(AddText.class.getResource("/res/bold.png")));
-		tglbtnNewToggleButton.setBounds(351, 91, 26, 23);
-		contentPanel.add(tglbtnNewToggleButton);
-		
-		JToggleButton toggleButton = new JToggleButton("");
-		toggleButton.setIcon(new ImageIcon(AddText.class.getResource("/res/italic.png")));
-		toggleButton.setBounds(387, 91, 26, 23);
-		contentPanel.add(toggleButton);
-		
-		JToggleButton toggleButton_1 = new JToggleButton("");
-		toggleButton_1.setIcon(new ImageIcon(AddText.class.getResource("/res/underline.png")));
-		toggleButton_1.setBounds(423, 91, 26, 23);
-		contentPanel.add(toggleButton_1);
-		
+		UnderlineToggleButton.setIcon(new ImageIcon(AddText.class.getResource("/res/underline.png")));
+		UnderlineToggleButton.setBounds(424, 92, 26, 23);
+		contentPanel.add(UnderlineToggleButton);
+
 		JLabel lblFontStyle = new JLabel("Font Style:");
-		lblFontStyle.setBounds(261, 91, 80, 15);
+		lblFontStyle.setBounds(262, 92, 80, 15);
 		contentPanel.add(lblFontStyle);
-		
+
 		JScrollPane scrollPane = new JScrollPane();
-		scrollPane.setBounds(43, 28, 207, 89);
+		scrollPane.setBounds(100, 28, 350, 40);
 		contentPanel.add(scrollPane);
-		
-		textArea = new JTextArea();
+
+		textArea = new JTextField();
 		scrollPane.setViewportView(textArea);
+		textFont=new Font("SansSerif",Font.PLAIN,12);
+		textArea.setFont(textFont);
+		textSize=12;
+		textType="SansSerif";
 		
-		JScrollPane scrollPane_1 = new JScrollPane();
-		scrollPane_1.setBounds(20, 179, 448, 80);
-		contentPanel.add(scrollPane_1);
-		
-		JPanel result_panel = new TextPanel();
-		scrollPane_1.setViewportView(result_panel);
-		
+		result_panel = new TextPanel();
+		result_panel.setBounds(80, 200, 348, 38);
+		contentPanel.add(result_panel);
+
 		JSeparator separator = new JSeparator();
 		separator.setBounds(0, 167, 494, 92);
 		contentPanel.add(separator);
-		
+
 		JButton btnCreate = new JButton("Create");
 		btnCreate.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				
+				textFont = new Font(textType, textArea.getFont().getStyle(), textSize); /* TODO */
+				textArea.setFont(textFont);
+				System.out.println(textArea.getFont().getStyle());
+				System.out.println(textSize);
+				result_panel.repaint();
 			}
 		});
 		btnCreate.setBounds(397, 134, 87, 23);
 		contentPanel.add(btnCreate);
-		
+
 		{
 			JPanel buttonPane = new JPanel();
 			buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
@@ -136,31 +171,65 @@ public class AddText extends JDialog {
 			}
 			{
 				JButton cancelButton = new JButton("Cancel");
+				cancelButton.addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent e) {
+						System.exit(0);
+					}
+				});
 				cancelButton.setActionCommand("Cancel");
 				buttonPane.add(cancelButton);
 			}
 		}
 	}
-	
-	public class TextPanel extends JPanel{
-		public TextPanel(){
-			
+	ActionListener btnAction=new ActionListener() {
+		public void actionPerformed(ActionEvent e) {
+			if (italicToggleButton.isSelected()) {
+				textFont = new Font(textType, Font.ITALIC, textSize); /* TODO */
+				textArea.setFont(textFont);
+			}
+			if (UnderlineToggleButton.isSelected()) {
+				textFont = new Font(textType, Font.CENTER_BASELINE, textSize); /* TODO */
+				textArea.setFont(textFont);
+			}
+			if (boldToggleButton.isSelected()) {
+				textFont = new Font(textType, Font.BOLD, textSize); /* TODO */
+				textArea.setFont(textFont);
+			}
 		}
+	};
+
+	public class TextPanel extends JPanel {
+		public TextPanel() {
+
+		}
+
+		public void getFont(Font font) {
+			bg.setFont(font);
+		}
+
 		public void paintComponent(Graphics g) {
-			buffer = new BufferedImage(330, 450, BufferedImage.TYPE_INT_RGB);
+			buffer = new BufferedImage(textArea.getWidth(), textArea.getHeight(), BufferedImage.TYPE_INT_RGB);
+			buffer=ScreenImage.createImage(textArea);
+			/*
 			bg = buffer.createGraphics();
 			bg.setPaint(Color.WHITE);
-			bg.fillRect(0, 0, 330, 450);
+			bg.fillRect(0, 0, textArea.getWidth(), textArea.getHeight());
 			bg.setPaint(Color.BLACK);
-			bg.drawString("123", 20, 20);
+			bg.drawString(textArea.getText(), 0, 20);
+			 */
+			/*
+			 * StringTokenizer st=new StringTokenizer(textArea.getText()); int
+			 * lineCount=0; while(st.hasMoreTokens()){
+			 * //.drawString(st.nextToken(),0, 0+lineCount * 10); lineCount++; }
+			 */
 			g.drawImage(buffer, 0, 0, null);
 			try {
 				Thread.sleep(33);
 			} catch (Exception e) {
 				e.printStackTrace();
 			}
-			
+
 		}
-		
+
 	}
 }
